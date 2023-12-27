9d1bc78995f8d707788c5e72b11d7de09ba59fa7
GitHub Action <action@github.com>
2023-12-27 06:10:58 +0000

    Initialize template repository proper
    Set package name to com.github.indigopolecat.bingobrewers
    Set project name to BingoBrewers
    Set modid to bingobrewers

--- src/main/java/com/github/indigopolecat/bingobrewers/ExampleMod.java
+++ src/main/java/com/github/indigopolecat/bingobrewers/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.indigopolecat.bingobrewers;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "bingobrewers", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/indigopolecat/bingobrewers/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/indigopolecat/bingobrewers/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.indigopolecat.bingobrewers.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
