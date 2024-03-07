2cab1f24f37a9ec0da281c0c01fba25081119de3
Ferox-Stylo <fabian.weigel1@web.de>
2024-03-07 22:09:14 +0100

    docs: add @param annotation for TooltipInfo constructor

--- src/main/java/com/github/indigopolecat/bingobrewers/TooltipInfo.java
+++ src/main/java/com/github/indigopolecat/bingobrewers/TooltipInfo.java
@@ -7,9 +7,18 @@ public class TooltipInfo {
     private final String extraCost;
     private final int extraCostIndex;
     private final int costIndex;
-
     private final int bingoPointsPrice;
 
+    /**
+     * Constructs a TooltipInfo object with the given parameters.
+     *
+     * @param name The name of the item.
+     * @param cost The cost per point of the item.
+     * @param extraCost The extra cost associated with the item, if any.
+     * @param costIndex The index in the tooltip where the cost per point should be inserted.
+     * @param extraCostIndex The index in the tooltip where the extra cost should be inserted.
+     * @param bingoPointsPrice The price of the item in Bingo Points.
+     */
     public TooltipInfo(String name, String cost, String extraCost, int costIndex, int extraCostIndex, int bingoPointsPrice) {
         this.name = name;
         this.costPerPoint = cost;
