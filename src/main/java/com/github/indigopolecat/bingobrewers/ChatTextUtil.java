86af1ea79d1687b178ef9f6198e2b27104907466
Ferox-Stylo <fabian.weigel1@web.de>
2024-04-02 20:56:42 +0200

    config: add code to display egg laying message

--- src/main/java/com/github/indigopolecat/bingobrewers/ChatTextUtil.java
+++ src/main/java/com/github/indigopolecat/bingobrewers/ChatTextUtil.java
@@ -3,9 +3,18 @@ package com.github.indigopolecat.bingobrewers;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class ChatTextUtil {
+import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import static com.github.indigopolecat.bingobrewers.BingoBrewersConfig.displayEggTimerReset;
+
+public class ChatTextUtil {
     public static boolean cancelLocRawMessage = false;
+
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
     @SubscribeEvent
     public void onChatMessage(ClientChatReceivedEvent event) {
         String message = event.message.getUnformattedText();
@@ -13,6 +22,12 @@ public class ChatTextUtil {
             event.setCanceled(true);
             cancelLocRawMessage = false;
             PlayerInfo.lastPositionUpdate = System.currentTimeMillis();
+        } else if (message.equals("You laid an egg!") && displayEggTimerReset) {
+            scheduler.schedule(() -> {
+                TitleHud titleHud = new TitleHud("You can lay an egg again", Color.WHITE.getRGB(), 1000);
+                ServerConnection serverConnection = new ServerConnection();
+                serverConnection.setActiveHud(titleHud);
+            }, 5, TimeUnit.SECONDS);
         }
     }
 }
