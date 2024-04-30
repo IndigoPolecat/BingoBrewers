200893faa6331566393c7cb2e4d7ac733d998810
Conutik <60240193+Conutik@users.noreply.github.com>
2024-04-30 21:29:19 +0300

    Fabric template

---
+++ src/main/java/com/conutik/bingobrewers/BingoBrewersClient.java
@@ -0,0 +1,29 @@
+package com.conutik.bingobrewers;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class BingoBrewersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println("Hello from CoflTestClient!");
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            System.out.println("Joined the server!");
+            if(MinecraftClient.getInstance().player != null) {
+                MinecraftClient.getInstance().player.sendMessage(Text.of("Hello!"), true);
+            }
+        });
+    }
+
+    @Subscribe
+    public void onSocketOpen(SocketOpen event) {
+        System.out.println("Socket opened!");
+    }
+}
