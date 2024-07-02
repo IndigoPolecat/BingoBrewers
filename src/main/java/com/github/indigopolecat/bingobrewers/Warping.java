bc2be3b9052c014f895adfb3fa5c19f2d856cd16
IndigoPolecat <115671621+IndigoPolecat@users.noreply.github.com>
2024-07-02 00:10:21 -0600

    Theoretical party kick mechanism

--- src/main/java/com/github/indigopolecat/bingobrewers/Warping.java
+++ src/main/java/com/github/indigopolecat/bingobrewers/Warping.java
@@ -3,39 +3,83 @@ package com.github.indigopolecat.bingobrewers;
 import com.github.indigopolecat.kryo.KryoNetwork;
 import net.hypixel.modapi.packet.impl.serverbound.ServerboundPartyInfoPacket;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.IChatComponent;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Warping {
     public static ConcurrentHashMap<String, String> accountsToWarp = new ConcurrentHashMap<>();
     public static String server;
-    public static long warpDelay;
     // when the party invite was sent out
     public static long inviteSent;
     public static boolean requestLiveParty = false;
     public static long lastPartyUpdate;
+    public static boolean waitingOnLocation;
+    public static boolean partyReady;
+    public static PARTY_PACKET_INTENT intent;
+
+    public static boolean kickParty;
+    public static int ticksSinceLastKick = 8;
+    public static int TICKS_BETWEEN_KICKS = 8;
 
     public static void warp() {
         requestLiveParty = false;
+        Minecraft.getMinecraft().thePlayer.sendChatMessage("/p warp");
     }
 
     public static void abort(boolean ineligible) {
+        kickParty = true;
+        BingoBrewers bb = new BingoBrewers();
+        bb.sendPacket(new ServerboundPartyInfoPacket());
+        intent = PARTY_PACKET_INTENT.VERIFY_MEMBERS_KICKED;
+
         KryoNetwork.AbortWarpTask abort = new KryoNetwork.AbortWarpTask();
         abort.ign = Minecraft.getMinecraft().thePlayer.getDisplayNameString();
         abort.ineligible = ineligible;
         ServerConnection.sendTCP(abort);
+        accountsToWarp.clear();
+        requestLiveParty = false;
+        partyReady = false;
+        waitingOnLocation = true;
     }
 
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent event) {
         if (event.phase.equals(TickEvent.Phase.END)) {
             if (requestLiveParty && lastPartyUpdate + 2000 < System.currentTimeMillis()) {
-                lastPartyUpdate = System.currentTimeMillis();
-                BingoBrewers bb = new BingoBrewers();
-                bb.sendPacket(new ServerboundPartyInfoPacket());
+                if (intent != PARTY_PACKET_INTENT.VERIFY_MEMBERS_KICKED) {
+                    lastPartyUpdate = System.currentTimeMillis();
+                    BingoBrewers bb = new BingoBrewers();
+                    bb.sendPacket(new ServerboundPartyInfoPacket());
+                    intent = PARTY_PACKET_INTENT.CHECK_MEMBERS_JOINED;
+                }
+            }
+            if (kickParty && ticksSinceLastKick >= TICKS_BETWEEN_KICKS) {
+                ticksSinceLastKick = 0;
+                String accountToKickUUID = accountsToWarp.keySet().toArray(new String[0])[0];
+                String accountToKick = accountsToWarp.get(accountToKickUUID);
+                Minecraft.getMinecraft().thePlayer.sendChatMessage("/p kick " + accountToKick);
+                accountsToWarp.remove(accountToKickUUID);
+                if (accountsToWarp.isEmpty()) {
+                    kickParty = false;
+                    ticksSinceLastKick = TICKS_BETWEEN_KICKS;
+                }
+
+            } else if (kickParty) {
+                ticksSinceLastKick++;
             }
         }
+    }
+
+    public enum PARTY_PACKET_INTENT {
+        VERIFY_MEMBERS_KICKED,
+        CHECK_MEMBERS_JOINED,
+        VERIFY_PARTY_EMPTY
     }
 }
--- src/main/java/com/github/indigopolecat/events/HypixelPackets.java
+++ src/main/java/com/github/indigopolecat/events/HypixelPackets.java
@@ -27,34 +27,36 @@ public class HypixelPackets {
 
     public static void onPartyInfoPacket(ClientboundPartyInfoPacket packet) {
         PlayerInfo.inParty = packet.isInParty();
-        if (PlayerInfo.inParty && PlayerInfo.registeredToWarp) {
-            if (!Warping.accountsToWarp.isEmpty()) {
-                Map<UUID, ClientboundPartyInfoPacket.PartyMember> party = packet.getMemberMap();
-                ArrayList<String> uuids = new ArrayList<>();
+        if (Warping.intent == Warping.PARTY_PACKET_INTENT.CHECK_MEMBERS_JOINED) {
+            if (PlayerInfo.inParty && PlayerInfo.registeredToWarp) {
+                if (!Warping.accountsToWarp.isEmpty()) {
+                    Map<UUID, ClientboundPartyInfoPacket.PartyMember> party = packet.getMemberMap();
+                    ArrayList<String> uuids = new ArrayList<>();
 
-                for (UUID uuid : party.keySet()) {
-                    if (party.get(uuid).getRole().equals(ClientboundPartyInfoPacket.PartyRole.LEADER)) continue;
-                    else uuids.add(uuid.toString());
-                }
+                    for (UUID uuid : party.keySet()) {
+                        if (party.get(uuid).getRole().equals(ClientboundPartyInfoPacket.PartyRole.LEADER)) continue;
+                        else uuids.add(uuid.toString());
+                    }
 
-                if (Warping.accountsToWarp.keySet().containsAll(uuids) && uuids.containsAll(Warping.accountsToWarp.keySet())) { // everyone is in the party
-                    Warping.warp();
-                } else if (Warping.accountsToWarp.keySet().containsAll(uuids) && Warping.inviteSent + 5000 < System.currentTimeMillis()) { // warp after 5 seconds even if the party isn't full
-                    Warping.warp();
-                } else if (!Warping.accountsToWarp.keySet().containsAll(uuids)) { // there is someone who isn't supposed to be warped in the party
-                    Warping.requestLiveParty = false;
-                    Warping.abort(true);
+                    if (Warping.accountsToWarp.keySet().containsAll(uuids) && uuids.containsAll(Warping.accountsToWarp.keySet())) { // everyone is in the party
+                        Warping.warp();
+                    } else if (Warping.accountsToWarp.keySet().containsAll(uuids) && Warping.inviteSent + 5000 < System.currentTimeMillis()) { // warp after 5 seconds even if the party isn't full
+                        Warping.warp();
+                    } else if (!Warping.accountsToWarp.keySet().containsAll(uuids)) { // there is someone who isn't supposed to be warped in the party
+                        Warping.requestLiveParty = false;
+                        Warping.abort(true);
+                    } else {
+                        Warping.requestLiveParty = true;
+                        Warping.lastPartyUpdate = System.currentTimeMillis();
+                        BingoBrewers bb = new BingoBrewers();
+                        bb.sendPacket(new ServerboundPartyInfoPacket());
+                    }
                 } else {
-                    Warping.requestLiveParty = true;
-                    Warping.lastPartyUpdate = System.currentTimeMillis();
-                    BingoBrewers bb = new BingoBrewers();
-                    bb.sendPacket(new ServerboundPartyInfoPacket());
+                    KryoNetwork.RegisterToWarpServer register = new KryoNetwork.RegisterToWarpServer();
+                    register.server = PlayerInfo.currentServer;
+                    register.unregister = true;
+                    ServerConnection.sendTCP(register);
                 }
-            } else {
-                KryoNetwork.RegisterToWarpServer register = new KryoNetwork.RegisterToWarpServer();
-                register.server = PlayerInfo.currentServer;
-                register.unregister = true;
-                ServerConnection.sendTCP(register);
             }
         }
     }
