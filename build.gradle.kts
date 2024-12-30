9ac996b0e399c4ed04a3b2736fd645dc64b01040
IndigoPolecat <115671621+IndigoPolecat@users.noreply.github.com>
2024-12-30 01:32:27 -0700

    Make gradle build script work using DGT, project still cannot be built

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,198 @@
 import org.apache.commons.lang3.SystemUtils
+import dev.deftu.gradle.utils.GameSide
+
+// I think these are using the wrong variables
+val baseGroup: String by project
+val mcVersion: String by project
+val version: String by project
+val mixinGroup = "$baseGroup.mixin"
+val modid: String by project
 
 plugins {
-    idea
+    //idea
     java
+    //id("dev.deftu.gradle.multiversion") version "2.12.2"
+    id("dev.deftu.gradle.tools") version "2.15.0"
+    id("dev.deftu.gradle.tools.resources") version "2.15.0"
+    id("dev.deftu.gradle.tools.bloom") version "2.15.0"
+    id("dev.deftu.gradle.tools.shadow") version "2.15.0"
+    id("dev.deftu.gradle.tools.minecraft.loom") version "2.15.0"
+    /*
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("gg.essential.loom") version "0.10.0.+"
     id("dev.architectury.architectury-pack200") version "0.1.3"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("net.kyori.blossom") version "1.3.1"
     id("signing")
+     */
 }
 
 
 
+toolkitLoomHelper {
+    // Adds OneConfig to our project
+    useOneConfig {
+        version = "1.0.0-alpha.47"
+        loaderVersion = "1.1.0-alpha.34"
+
+        usePolyMixin = true
+        polyMixinVersion = "0.8.4+build.2"
+
+        +"commands"
+        +"events"
+        +"hud"
+        +"ui"
+    }
+
+    useDevAuth()
+    // Removes the server configs from IntelliJ IDEA, leaving only client runs.
+    // If you're developing a server-side mod, you can remove this line.
+    disableRunConfigs(GameSide.SERVER)
+
+    // Sets up our Mixin refmap naming
+    if (!mcData.isNeoForge) {
+        useMixinRefMap(modData.id)
+    }
+
+    // Adds the tweak class if we are building legacy version of forge as per the documentation (https://docs.polyfrost.org)
+    if (mcData.isLegacyForge) {
+        useTweaker("org.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker", GameSide.CLIENT)
+        useForgeMixin(modData.id) // Configures the mixins if we are building for forge, useful for when we are dealing with cross-platform projects.
+    }
+
+}
+
+loom {
+    // regex pattern to filter out spam errors from hypixel
+    log4jConfigs.from(file("log4j2.xml"))
+
+    runConfigs {
+        "client" {
+            if (SystemUtils.IS_OS_MAC_OSX) {
+                // This argument causes a crash on macOS
+                vmArgs.remove("-XstartOnFirstThread")
+                programArgs("--tweakClass", "cc.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker")
+                property("mixin.debug.export", "true")
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        output.setResourcesDir(sourceSets.main.flatMap { it.java.classesDirectory })
+    }
+
+    val dummy by creating
+    main {
+        dummy.compileClasspath += compileClasspath
+        compileClasspath += dummy.output
+        output.setResourcesDir(java.classesDirectory)
+    }
+}
+
+// Dependencies:
+
+repositories {
+    mavenCentral()
+    maven("https://repo.polyfrost.org/releases")
+    maven("https://repo.spongepowered.org/maven/")
+    // If you don't want to log in with your real minecraft account, remove this line
+    maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
+    maven("https://repo.nea.moe/releases")
+    //maven("https://repo.hypixel.net/repository/Hypixel/")
+}
+
+val shadowImpl: Configuration by configurations.creating {
+    configurations.implementation.get().extendsFrom(this)
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:1.8.9")
+    //mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
+    forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
+
+    // If you don't want mixins, remove these lines
+    shadowImpl("org.spongepowered:mixin:0.7.11-SNAPSHOT") {
+        isTransitive = false
+    }
+    annotationProcessor("org.spongepowered:mixin:0.8.5-SNAPSHOT")
+
+    // If you don't want to log in with your real minecraft account, remove this line
+    runtimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.1.2")
+    implementation("com.google.code.gson:gson:2.9.1")
+    shadowImpl("com.esotericsoftware:kryonet:2.22.0-RC1")
+    // Basic OneConfig dependencies for legacy versions. See OneConfig example mod for more info
+    modCompileOnly("cc.polyfrost:oneconfig-1.8.9-forge:0.2.2-alpha+") // Should not be included in jar
+    // include should be replaced with a configuration that includes this in the jar
+    shadowImpl("cc.polyfrost:oneconfig-wrapper-launchwrapper:1.0.0-beta+") // Should be included in jar
+    shadowImpl("moe.nea:libautoupdate:1.3.1") // Should be included in jar
+    implementation("net.hypixel:mod-api:1.0")
+
+}
+
+tasks.withType(JavaCompile::class) {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType(Jar::class) {
+    archiveBaseName.set(modid)
+    manifest.attributes.run {
+        this["FMLCorePluginContainsFMLMod"] = "true"
+        this["ForceLoadAsMod"] = "true"
+        this["TweakOrder"] = 0
+
+        // If you don't want mixins, remove these lines
+        this["TweakClass"] = "cc.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker"
+        this["MixinConfigs"] = "mixins.$modid.json"
+    }
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    inputs.property("mcversion", mcVersion)
+    inputs.property("modid", modid)
+    inputs.property("mixinGroup", mixinGroup)
+
+    filesMatching(listOf("mcmod.info", "mixins.$modid.json")) {
+        expand(inputs.properties)
+    }
+
+    rename("(.+_at.cfg)", "META-INF/$1")
+}
+
+/*
+val remapJar by tasks.named<net.fabricmc.loom.task.RemapJarTask>("remapJar") {
+    archiveClassifier.set("")
+    from(tasks.shadowJar)
+    input.set(tasks.shadowJar.get().archiveFile)
+}
+ */
+
+tasks.jar {
+    archiveClassifier.set("without-deps")
+    destinationDirectory.set(layout.buildDirectory.dir("badjars"))
+}
+
+/*
+tasks.shadowJar {
+    destinationDirectory.set(layout.buildDirectory.dir("badjars"))
+    archiveClassifier.set("all-dev")
+    configurations = listOf(shadowImpl)
+    doLast {
+        configurations.forEach {
+            println("Copying jars into mod: ${it.files}")
+        }
+    }
+    //relocate("com.google.gson", "com.github.indigopolecat")
+
+    // If you want to include other dependencies and shadow them, you can relocate them in here
+    fun relocate(name: String) = relocate(name, "com.github.indigopolecat.$name")
+}
+ */
+//tasks.assemble.get().dependsOn(tasks.shadowJar)
+//tasks.assemble.get().dependsOn(tasks.remapJar)
+
+/*
 //Constants:
 
 val baseGroup: String by project
@@ -29,8 +210,7 @@ java {
 loom {
     launchConfigs.named("client") {
         // Loads OneConfig in dev env. Replace other tweak classes with this, but keep any other attributes!
-        arg("--tweakClass", "cc.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker")
-    }
+        arg("--tweakClass", "org.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker")    }
     log4jConfigs.from(file("log4j2.xml"))
     launchConfigs {
         "client" {
@@ -64,7 +244,6 @@ loom {
 
 sourceSets.main {
     output.setResourcesDir(sourceSets.main.flatMap { it.java.classesDirectory })
-
 }
 
 sourceSets {
@@ -181,6 +360,7 @@ tasks.shadowJar {
 }
 tasks.assemble.get().dependsOn(tasks.shadowJar)
 tasks.assemble.get().dependsOn(tasks.remapJar)
+ */
 
 
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://oss.sonatype.org/content/repositories/snapshots")
+        maven("https://maven.deftu.dev/releases")
+        maven("https://maven.deftu.dev/snapshots")
         maven("https://maven.architectury.dev/")
         maven("https://maven.fabricmc.net")
         maven("https://maven.minecraftforge.net/")
@@ -19,7 +21,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
+    //id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
 }
 
 
