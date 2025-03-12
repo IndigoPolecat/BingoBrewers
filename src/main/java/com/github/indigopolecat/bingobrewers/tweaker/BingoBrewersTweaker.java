f15c6ae60ae1ba745b10a693d65fbcb360a736f7
IndigoPolecat <115671621+IndigoPolecat@users.noreply.github.com>
2025-03-11 23:41:17 -0600

    Add NEU tweaker to fix mod not loading with essential. Add copy of LGPL license since NEU is licensed under LGPL.

--- src/main/java/com/github/indigopolecat/bingobrewers/tweaker/BingoBrewersTweaker.java
+++ src/main/java/com/github/indigopolecat/bingobrewers/tweaker/BingoBrewersTweaker.java
@@ -12,8 +12,8 @@ public class BingoBrewersTweaker implements ITweaker {
     @Override
     public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
         List<String> tweakClasses = (List<String>) Launch.blackboard.get("TweakClasses"); // always of type List<String>
-        tweakClasses.add("cc.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker");
         tweakClasses.add("net.hypixel.modapi.tweaker.HypixelModAPITweaker");
+        tweakClasses.add("com.github.indigopolecat.bingobrewers.tweaker.ModLoadingTweaker");
     }
 
     @Override
